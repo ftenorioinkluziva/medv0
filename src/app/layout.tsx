8a05957e6a7c9c4ca9f36886c90724a9f9e5cba5
ftenorioinkluziva <fernando.tenorio@inkluziva.org>
2026-03-23 23:37:02 -0300

    feat: initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata, Viewport } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
 import './globals.css'
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: 'SAMI',
@@ -22,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="pt-BR" className="dark" suppressHydrationWarning>
+    <html lang="pt-BR" className={cn("dark", "font-sans", geist.variable)} suppressHydrationWarning>
       <body className={`${GeistSans.variable} ${GeistMono.variable} antialiased`}>
         {children}
       </body>
